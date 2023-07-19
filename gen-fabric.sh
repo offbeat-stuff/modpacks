55a5a2d2ffdf669a8a0f516a3d39dbd98408e0d1
ZenXArch mobile <offbeat-stuff@github.com>
2023-07-19 15:31:47 +0530

    update script

--- gen-fabric.sh
+++ gen-fabric.sh
@@ -4,14 +4,23 @@ copy_mods () {
   cp $1/mods/*.pw.toml temp/mods
 }
 
+gen_mrpack () {
+  cd temp
+  touch index.toml
+  packwiz refresh
+  packwiz update -a
+  packwiz modrinth export
+  cp ./*.mrpack ../$1.mrpack
+  cd ..
+}
+
 cp -r config temp
 mkdir temp/mods
 
 copy_mods fabric-base
 copy_mods perf-base
 
-cd temp
-touch index.toml
-packwiz refresh
-packwiz update -a
-packwiz modrinth export
+gen_mrpack fabric
+
+copy_mods exp-perf-base
+gen_mrpack fabric-exp
