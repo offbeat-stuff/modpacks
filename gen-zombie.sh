f9db813d2ee9c5ba5bb368530f493e43dcdb7aad
Artix Kai <artix@github.com>
2023-08-14 16:17:25 +0530

    new modpack ig

---
+++ gen-zombie.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rm -rf temp/
+
+copy_mods () {
+  cp $1/mods/*.pw.toml temp/mods
+}
+
+copy_resourcepacks () {
+  cp $1/resourcepacks/*.pw.toml temp/resourcepacks
+}
+
+gen_mrpack () {
+  cd temp
+  touch index.toml
+  packwiz refresh
+  packwiz update -a
+  packwiz modrinth export
+  cp ./*.mrpack ../$1.mrpack
+  cd ..
+}
+
+cp -r config temp
+mkdir temp/mods
+mkdir temp/resourcepacks
+
+cp quilt-base/pack.toml temp/
+
+copy_mods fabric-base
+copy_mods perf-base
+copy_mods zombie-base
+copy_resourcepacks zombie-base
+
+gen_mrpack zombie
